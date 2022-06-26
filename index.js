4f58c2a4932055b55171d37f6c8641e6352e4d1b
Abiodun <aabiodunawoyemi@gmail.com>
2022-06-26 19:16:25 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function cipherRot13(str) {
+    str = str.toUpperCase();
+    return str.replace(/[A-Z]/g, rot13);
+  
+    function rot13(correspondance) {
+        const charCode = correspondance.charCodeAt();
+        //A = 65, Z = 90
+        return String.fromCharCode(
+            ((charCode + 13) <= 90) 
+            ? charCode + 13
+            : (charCode + 13) % 90 + 64
+        );
+      
+    }
+}
+
+console.log(cipherRot13("ABIODUN"))
